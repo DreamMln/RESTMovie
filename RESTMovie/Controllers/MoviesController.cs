248c9761581a8cb9b983e188f270fca952e37fd5
DreamMln <maix2374@edu.zealand.dk>
2022-06-09 21:13:10 +0200

    Add project files.

---
+++ RESTMovie/Controllers/MoviesController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieLib;
+using RESTMovie.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTMovie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        //reference til manager class
+        //instans
+        //opretter objektet 
+        private MoviesManager _manager = new MoviesManager();
+
+        // GET: api/<MoviesController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        //action result så statuskoderne kan anvendes
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            IEnumerable<Movie> result = _manager.GetAll();
+            if (result.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // GET api/<MoviesController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Movie>> Get(int id)
+        {
+            Movie result = _manager.GetById(id);
+            if (result == null)
+            {
+                return NotFound("No such Movie with, Id: " + id);
+            }
+            return Ok(result);
+        }
+
+        // GET api/<MoviesController>/5
+        //filter - lengthinminutes
+        //Is able to filter the result by either:
+        //Name containing the substring (case-insensitive)
+        //from query - binding
+        [HttpGet("search")]
+        public IEnumerable<Movie> GetF([FromQuery] int lengthInMinutes)
+        {
+            return _manager.GetFilter(lengthInMinutes);
+        }
+
+
+        // POST api/<MoviesController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public ActionResult<Movie> Post([FromBody] Movie newMovie)
+        {
+            Movie result = _manager.AddMovie(newMovie);
+            if (result == null)
+            {
+                return null;
+            }
+            //ellers så bliver der tilføjet/created en Movie til listen
+            return Created($"/api/movies/{result.Id}", result);
+        }
+
+        // PUT api/<MoviesController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<MoviesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ RESTMovie/Managers/MoviesManager.cs
@@ -0,0 +1,54 @@
+﻿using MovieLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTMovie.Managers
+{
+    public class MoviesManager
+    {
+        /// <summary>
+        /// hvis vi ikke kan oprette den statiske liste
+        /// så kan vi lave en constructor og initialisere listen
+        /// i. Det kan være en løsning
+        /// </summary>
+        
+        private static int _nextId = 1;
+        private static List<Movie> _movies = new List<Movie>
+        {
+            new Movie { Id = _nextId++, LengthInMinutes = 120, CountryOfOrigin = "USA", Name = "Dune"},
+            new Movie { Id = _nextId++, LengthInMinutes = 155, CountryOfOrigin = "California", Name = "LOTR"},
+            new Movie { Id = _nextId++, LengthInMinutes = 133, CountryOfOrigin = "Denmark", Name = "Kære Børn"}
+        };
+        
+        public List<Movie> GetAll()
+        {
+            List<Movie> result = new List<Movie>(_movies);
+            return result;
+        }
+
+        public Movie GetById(int id)
+        {
+           return _movies.Find(m => m.Id == id);
+        }
+        //filter
+        public IEnumerable<Movie> GetFilter(int filter)
+        {
+            IEnumerable<Movie> result = _movies.Where(x => x.LengthInMinutes < filter);
+            return result;
+        }
+
+        public Movie AddMovie(Movie addMovie)
+        {
+            // der er ikke nogle objekt
+            if (addMovie == null)
+            {
+                return null;
+            }
+            addMovie.Id = _nextId++;
+            _movies.Add(addMovie);
+            return addMovie;
+        }
+    }
+}
